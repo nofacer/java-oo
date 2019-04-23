3cd316a19c4ddf175d00675dce6c7766c807f3bb
nofacer <nofacer@outlook.com>
2019-04-23 21:20:15 +0800

    test1

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,17 @@
 package practice01;
 
 public class Person {
+    private String name;
+    private int age;
+    public Person(String myName,int myAge){
+        name=myName;
+        age=myAge;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+
 }
