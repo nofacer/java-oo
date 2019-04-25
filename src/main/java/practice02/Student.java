bb62267fc1571ac4757d3274c7aaea3ec458cc2a
nofacer <nofacer@outlook.com>
2019-04-25 21:20:50 +0800

    02

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,19 @@
 package practice02;
 
-public class Student {
+public class Student extends Person {
+    private int klass;
+
+    public Student(String myName, int myAge, int myKlass) {
+        super(myName, myAge);
+        klass = myKlass;
+    }
+
+    public int getKlass(){
+        return klass;
+    }
+
+    public String introduce() {
+        return "I am a Student. I am at Class "+klass+".";
+    }
+
 }
