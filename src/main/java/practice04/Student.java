cf7188b5db0ac3681fd09fdaf625ecdcabbce7f2
nofacer <nofacer@outlook.com>
2019-04-26 13:46:48 +0800

    04

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,19 @@
 package practice04;
 
-public class Student {
+public class Student extends Person {
+    private int klass;
+
+    public Student(String myName, int myAge, int myKlass) {
+        super(myName, myAge);
+        klass = myKlass;
+    }
+
+    public int getKlass(){
+        return klass;
+    }
+
+    public String introduce() {
+        return super.introduce()+" I am a Student. I am at Class "+klass+".";
+    }
+
 }
