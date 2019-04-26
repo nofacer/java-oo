32edcd17ec54b9c055e8b78659e2176e5f5d6bc8
nofacer <nofacer@outlook.com>
2019-04-26 14:57:47 +0800

    06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,28 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int klass = -1;
+
+    public Teacher(String myName, int myAge, int myKlass) {
+        super(myName, myAge);
+        klass = myKlass;
+    }
+
+    public Teacher(String myName, int myAge) {
+        super(myName, myAge);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        if (klass != -1) {
+            return super.introduce() + " I am a Teacher. I teach Class "+klass+".";
+        }
+        else{
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+
+    }
 }
