54d521124e2d02667a2791bd052a90f3f0ad66c5
nofacer <nofacer@outlook.com>
2019-04-26 19:51:54 +0800

    10

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,40 @@
 package practice10;
 
+import java.util.ArrayList;
+
 public class Klass {
+    private int klass;
+    private Student leaderStudent;
+    private ArrayList<Student> studentList = new ArrayList<Student>();
+
+    public Klass(int myKlass) {
+        klass = myKlass;
+    }
+
+    public int getNumber() {
+        return klass;
+    }
+
+    public String getDisplayName() {
+        return "Class " + klass;
+    }
+
+    public void assignLeader(Student student) {
+        if(studentList.contains(student)){
+            leaderStudent = student;
+            student.leader = true;
+        }
+        else{
+            System.out.println("It is not one of us.");
+        }
+        
+    }
+
+    public Student getLeader() {
+        return leaderStudent;
+    }
+
+    public void appendMember(Student student) {
+        studentList.add(student);
+    }
 }
--- src/main/java/practice10/Person.java
+++ src/main/java/practice10/Person.java
@@ -1,4 +1,40 @@
 package practice10;
 
 public class Person {
+    private int eyed;
+    private String name;
+    private int age;
+
+    public Person(int myId, String myName, int myAge) {
+        eyed = myId;
+        name = myName;
+        age = myAge;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Person) {
+            Person person = (Person) obj;
+            if (eyed == person.eyed) {
+                return true;
+            }
+            else{
+                return false;
+            }
+            
+        }
+        return false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
 }
